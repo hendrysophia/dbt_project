4c73e745cc90415a0f717f89ab282c6d90262361
Sophia Agnus <hendrysophia03@gmail.com>
2024-04-22 13:59:54 +0530

    Update mview_weekly_sales.sql

--- models/marts/core/mview_weekly_sales.sql
+++ models/marts/core/mview_weekly_sales.sql
@@ -1,5 +1,5 @@
 with
-    orders as (select * from {{ ref("stg_hier_clnd") }}),
+    calendar as (select * from {{ ref("stg_hier_clnd") }}),
 
     prod as (select * from {{ ref("stg_hier_prod") }}),
 
@@ -21,7 +21,7 @@ with
             sum(sales_dollars),
             sum(discount_dollars)
         from transactions as t1
-        join orders as t2 on t1.date_id = t2.date_id
+        join calendar as t2 on t1.date_id = t2.date_id
         join prod as t3 on t3.sku_id = t1.sku_id
         join pricetype as t4 on t4.price_substate_id = t1.price_substate_id
         join possite as t5 on t5.pos_site_id = t1.pos_site_id
